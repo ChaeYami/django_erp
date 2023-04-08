294da558184c2ff4fb0e7f8f82fc94467c279fbe
Chaeyeon Seo <ssallyseo@gmail.com>
2023-04-09 04:40:49 +0900

    출고량 에러처리

--- erp/products/views.py
+++ erp/products/views.py
@@ -51,9 +51,9 @@ def inbound_create(request):
             return redirect('/sign-in')
     elif request.method == 'POST':
         product_code = request.POST.get('product_code', '')
-        inbound = int(request.POST.get('inbound', ''))
-        if product_code == '' or inbound == '':
-            return render(request, 'products/inventory.html', {'error': 'Please fill all the fields'})
+        inbound = request.POST.get('inbound', '')
+        if product_code == '' or inbound == '': # 입력된 값이 없을 때
+            return render(request, 'products/inbound_create.html', {'error': '내용을 입력해주세요'})
         else:
             
             inventory = Inventory.objects.get(product_code=product_code)
@@ -77,10 +77,13 @@ def outbound_create(request):
     elif request.method == 'POST':
         product_code = request.POST.get('product_code', '')
         outbound = request.POST.get('outbound', '')
-        if product_code == '' or outbound == '':
-            return render(request, 'products/inventory.html', {'error': 'Please fill all the fields'})
+        if product_code == '' or outbound == '': # 입력된 값이 없을 때
+            return render(request, 'products/outbound_create.html', {'error': '내용을 입력해주세요'})
         else:
             inventory = Inventory.objects.get(product_code=product_code)
+            
+            if outbound > inventory.stock:  # 출고하려는 수량이 재고보다 많을 경우
+                return render(request, 'products/inventory.html', {'error': '출고량은 재고량보다 많을 수 없습니다.'})
             
             inventory.stock -= int(outbound) # 재고량 감소
             inventory.save()
